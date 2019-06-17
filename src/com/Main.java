94253ddcf6f241d32bdef52c82ff117b1c67dc23
DemkoRob3rt <dr@dr-Lenovo-G580>
2019-06-17 13:20:02 +0200

    Elvileg oké

---
+++ src/com/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com;
+
+/**
+ * @author dr
+ * 
+ */
+public class Main {
+
+	public Main() {
+		System.out.println("Itt");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}
